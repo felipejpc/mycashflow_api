ad5e813ddde5ce80d8dfcfa0ff218162858bc0b2
felipejpc <felipejpc@yahoo.com.br>
2017-06-24 16:55:51 -0400

    adding version 2 for api

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api'}, path: '/' do
-    namespace :v1, path: '/', constraints: ApiVersionConstraint.new(apiversion: 1, default: true) do
+    namespace :v1, path: '/', constraints: ApiVersionConstraint.new(apiversion: 1) do
       resources :users, only:[ :show, :create, :update, :destroy ]
       resources :sessions, only:[ :create, :destroy ]
       resources :accounts, only: [ :index, :show, :create, :update, :destroy ]
     end
+    namespace :v2, path: '/', constraints: ApiVersionConstraint.new(apiversion: 2, default: true) do
+      resources :users, only:[ :show, :create, :update, :destroy ]
+      resources :sessions, only:[ :create, :destroy ]
+      resources :accounts, only: [ :index, :show, :create, :update, :destroy ]
+    end  
   end
 
 end
